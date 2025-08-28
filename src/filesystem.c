c2c784a338ceba9e13d48c6fd186d8d68a462060
adam sh <47113374+adamshahbari@users.noreply.github.com>
2025-08-28 17:39:38 +0300

    Add filesystem.c (filesystem implementation)

---
+++ src/filesystem.c
@@ -0,0 +1,11 @@
+#include "filesystem.h"
+#include <stdio.h>
+
+void init_filesystem() {
+    printf("Filesystem initialized.\n");
+}
+
+void list_files() {
+    printf("file1.txt\n");
+    printf("file2.txt\n");
+}
